721c2295da0d00ba44dfd225f60ef52f52ed2807
iganna <igolkinaanna11@gmail.com>
2017-11-17 14:33:20 +0300

    dynamic convolution

--- test_06_distr0030.py
+++ test_06_distr0030.py
@@ -3,16 +3,12 @@ from multiprocess import Pool
 
 
 
-
 def cdfs(i_distr):
 
-    def val_prob(indiv, probs, weights):
-        prob_multiply = list(map(lambda i: probs[i] if indiv[i] == '1' else (1 - probs[i]),
+    def calc_prob(indiv, probs):
+        prob_multiply = list(map(lambda i: probs[i] if indiv[i] == 1 else (1 - probs[i]),
                                  range(30)))
-        val_multiply = list(map(lambda i: weights[i] if indiv[i] == '1' else 0,
-                                 range(30)))
-        return np.prod(prob_multiply), sum(val_multiply)
-
+        return np.prod(prob_multiply)
 
     file_dir_pw = 'test_examples_n0030/'
     pw = np.loadtxt(file_dir_pw + 'distr' + str(i_distr).zfill(2) + '_pw.txt')
@@ -20,22 +16,31 @@ def cdfs(i_distr):
     weights = pw[1]
 
     target_indivs = np.loadtxt(file_dir_pw + 'distr' + str(i_distr).zfill(2) + '_pop.txt').astype(int)
-
     indiv_values = np.zeros(2 ** 30)
     indiv_probs = np.zeros(2 ** 30)
-    for indiv_id in range(0, 2 ** 30):
-        indiv = bin(indiv_id)[2:]
-        indiv += '0' * (30 - len(indiv))
-        indiv_probs[indiv_id], indiv_values[indiv_id] = val_prob(indiv, probs, weights)
 
-    target_values = list(map(lambda x: np.dot(x, weights), target_indivs))
+    indiv_values[0:2] = [0, weights[0]]
+    indiv_probs[0] = calc_prob(np.zeros(30), probs)
+    indiv_probs[1] = calc_prob(np.append([1], np.zeros(29)), probs)
 
-    cdfs = list(map(lambda x: sum(indiv_probs[indiv_values <= x]), target_values))
 
-    file_dir_res = 'res_examples_n0030/'
+    for i in range(1,30):
+        print(i)
+        n = 2 ** i
+        indiv_values[n:2*n] = indiv_values[0:n] + weights[i]
+        indiv_probs[n:2 * n] = indiv_probs[0:n] / (1 - probs[i]) * probs[i]
 
-    np.savetxt(file_dir_res + 'distr' + str(i_distr).zfill(2) + '_exact.txt',
-               cdfs, fmt='%.16f')
+
+
+    print(sum(indiv_probs))
+    # target_values = list(map(lambda x: np.dot(x, weights), target_indivs))
+    #
+    # cdfs = list(map(lambda x: sum(indiv_probs[indiv_values <= x]), target_values))
+    #
+    # file_dir_res = 'res_examples_n0030/'
+    #
+    # np.savetxt(file_dir_res + 'distr' + str(i_distr).zfill(2) + '_exact.txt',
+    #            cdfs, fmt='%.16f')
 
     print('Proc', i_distr, 'is ended')
 
