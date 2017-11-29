0075c061e8c94e58027ffad4edb3c7b70de47636
iganna <igolkinaanna11@gmail.com>
2017-11-29 14:01:35 +0300

    permutations

--- test_07_permutations.py
+++ test_07_permutations.py
@@ -3,7 +3,7 @@ import bernmix_double.bernmix_double as bmd
 from multiprocess import Pool
 
 
-def cdfs(i_distr):
+def cdfs_calc(i_distr):
 
     file_dir_pw = 'test_examples_n0100/'
     pw = np.loadtxt(file_dir_pw + 'distr' + str(i_distr).zfill(2) + '_pw.txt')
@@ -20,7 +20,7 @@ def cdfs(i_distr):
     cdfs = list(map(lambda target_indiv: bmd.cdf_permutations(probs, weights, target_indiv, n_permut), target_indivs))
 
     np.savetxt(file_dir_res + 'distr' + str(i_distr).zfill(2)  +
-               '_permut' + str(n_solutions).zfill(4) + '.txt',
+               '_permut' + str(n_permut).zfill(4) + '.txt',
                [np.array(cdfs)], fmt='%.16f')
 
 
@@ -30,5 +30,5 @@ n_disrt = 20
 
 with Pool(10) as workers:
     pmap = workers.map
-    pmap(cdfs, range(n_disrt))
+    pmap(cdfs_calc, range(n_disrt))
 
