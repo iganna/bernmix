fa865efc44825cede7a1ef33ef75aefd019df115
iganna <igolkinaanna11@gmail.com>
2017-12-06 19:45:38 +0300

    timig python

--- test_08_timing.py
+++ test_08_timing.py
@@ -3,9 +3,10 @@ import bernmix_int.bernmix_int as bmi
 from scipy.stats import binom
 import math
 import time
-
+import glob
 
 from poibin import PoiBin
+from multiprocess import Pool
 
 # def MN(p1, p2):
 #     return max(abs(p1-p2))
@@ -52,32 +53,60 @@ from poibin import PoiBin
 # list(a)
 
 
-for n in [500,1000,5000,10000,25000,50000,100000,150000,200000,250000,300000,350000,400000,450000,500000,550000,650000,750000,900000,1000000]:
-    weights = np.ones(n)
-    n_repeat = 100
-    t1 = 0
-    t2 = 0
-    for _ in range(n_repeat):
-        probs = np.random.rand(n)
+# #for n in [500,1000,5000,10000,25000,50000,100000,150000,200000,250000,300000,350000,400000,450000,500000,550000,650000,750000,900000,1000000]:
+# for n in [500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]:
+#     weights = -np.ones(n)
+#     n_repeat = 10
+#     t1 = 0
+#     t2 = 0
+#     for _ in range(n_repeat):
+#         probs = np.random.rand(n)
+#
+#         start1 = time.time()
+#         d = PoiBin(probs)
+#         pmf_exact = d.pmf(range(0, n + 1))
+#
+#         end1 = time.time()
+#         t1 += end1 - start1
+#
+#
+#         start2 = time.time()
+#         pmf_bermnix = bmi.pmf(probs, weights)
+#         end2 = time.time()
+#         t2 += end2 - start2
+#     t1 /= n_repeat
+#     t2 /= n_repeat
+#     print(t1, t2)
 
-        start1 = time.time()
-        d = PoiBin(probs)
-        pmf_exact = d.pmf(range(0, n + 1))
 
-        end1 = time.time()
-        t1 += end1 - start1
+##======================================
 
 
-        start2 = time.time()
-        pmf_bermnix = bmi.pmf(probs, weights)
-        end2 = time.time()
-        t2 += end2 - start2
-    t1 /= n_repeat
-    t2 /= n_repeat
+
+def timing(file):
+
+    pw = np.loadtxt(file)
+    probs = pw[0]
+    weights = pw[1]
+    n = len(weights)
+
+    start1 = time.time()
+    d = PoiBin(probs)
+    pmf_wieghted = bmi.pmf(probs, weights)
+    end1 = time.time()
+    t1 = end1 - start1
+
+    weights = np.zeros(n)
+    start2 = time.time()
+    pmf_unweighted = bmi.pmf(probs, weights)
+    end2 = time.time()
+    t2 = end2 - start2
     print(t1, t2)
 
 
 
-
-
+files = glob.glob('timing/data_for_timing/*')
+with Pool(10) as workers:
+    pmap = workers.map
+    pmap(timing, files)
 
