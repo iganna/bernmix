ee276257053721759cc4e457ff88fdc09cdfcfba
iganna <igolkinaanna11@gmail.com>
2018-03-11 15:13:30 +0300

    documentation

--- bernmix.py
+++ bernmix.py
@@ -1,186 +1,293 @@
-
-
+"""
+This module includes tools to compute PMFs and CDFs for weighted sum of Bernoulli RVs
+"""
 
 __author__ = "Anna Igolkina"
 __license__ = "MIT"
-__version__ = ""
+__version__ = "0.1"
 __maintainer__ = "Anna Igolkina"
 __email__ = "igolkinaanna11@gmail.com"
 __status__ = "Development"
 
-__all__ = ["bernmix_pmf_int", "bernmix_cdf_double"]
+__all__ = ["normalise_params",
+           "bernmix_pmf_int",
+           "bernmix_cdf_int",
+           "bernmix_cdf_double",
+           "permut_cdf",
+           "conv_all_outcomes",
+           "conv_pmf_int"
+           ]
 
-import bernmix_double.bernmix_double as bmd
-import bernmix_int.bernmix_int as bmi
 import numpy as np
+
+import bernmix_int.bernmix_int as bmi
+import bernmix_double.bernmix_double as bmd
+import bernmix_double.bernmix_correction as bmc
+import bernmix_control as control
+
 
 def normalise_params(probs, weights):
     """
-    This function normalises parameters of probabilities and weights as to make weights positive
+    This function normalises parameters of probabilities and weights
+    as to make weights positive
     :param probs: vector of probabilities
     :param weights: vector of weights
-    :return: sum of negative weights
+    :return: tuple of * new probabilities
+                      * new weights
+                      * bias
     """
-    sum_neg_weights = sum([w for w in weights if w < 0])
 
-    probs = [p * (w > 0) + (1 - p) * (w < 0) for p, w in zip(probs, weights)]
-    weights = [abs(w) for w in weights]
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    control.lengths(weights, probs)
+    # ----------------------------------------------
 
-    # if some weights == 0
-    # if some probabilities == 0
+    sum_bias = sum([w for w in weights if w < 0])
 
-    return probs, weights, sum_neg_weights
+    # BRV with negative weight is transformed to BRV with opposite probability
+    probs = np.array([p * (w > 0) + (1 - p) * (w < 0)
+                      for p, w in zip(probs, weights)])
+    weights = np.array([abs(w) for w in weights])
 
-def exact_int_pmf(probs, weights):
-    """
-    This function caclulates pmf fpr each individual by convolution
-    :param probs: vector of probabilities
-    :param weights: vector of weights
-    :return: probabilities and outcomes
-    """
-    if len(probs) != len(weights):
-        raise ValueError('Vectors of probabilities and weights must be the same length')
-    if len(probs) == 0:
-        raise ValueError('Sum of BRVs should contain at least one RV')
+    # Remain only significant terms:
+    # if some weights or probabilities equal to zero - remove
+    idx_significant = not ((probs == 0) | (weights == 0))
+    probs = probs[idx_significant]
+    weights = weights[idx_significant]
 
-    prob_indiv, outcomes = exact_prob_indiv(probs, weights)
-    pmf_bm = [sum(prob_indiv[outcomes == i]) for i in range(0, sum(weights) + 1)]
-    return pmf_bm
+    # Remove constant RVs : BRVs with probabilities equal to 1
+    # and change bias
+    idx_const = probs == 1
+    sum_bias += weights(idx_const)
+    probs = probs[not idx_const]
+    weights = weights[not idx_const]
 
-
-def exact_prob_indiv(probs, weights):
-    """
-    This function caclulates probabilities for each outcome
-    :param probs: vector of probabilities
-    :param weights: vector of weights
-    :return: probabilities and outcomes
-    """
-    def comp_indiv_prob(indiv, probs):
-        """ Compute probability for an individual """
-        n = len(probs)
-        prob_multiply = list(map(lambda i: probs[i] if indiv[i] == 1 else (1 - probs[i]),
-                                 range(n)))
-        return np.prod(prob_multiply)
-
-    if len(probs) != len(weights):
-        raise ValueError('Vectors of probabilities and weights must be the same length')
-    if len(probs) == 0:
-        raise ValueError('Sum of BRVs should contain at least one RV')
-
-    n = len(probs)
-    # initialise size of outcomes
-    outcomes = np.zeros(2 ** n)
-    prob_indiv = np.zeros(2 ** n)
-
-    # initialise two first of outcomes (0,0,...0) and (1,0,...0)
-    outcomes[0:2] = [0, weights[0]]
-    prob_indiv[0] = comp_indiv_prob(np.zeros(n), probs)
-    prob_indiv[1] = comp_indiv_prob(np.append([1], np.zeros(n-1)), probs)
-
-    for i in range(1, n):
-        n = 2 ** i
-        outcomes[n:2 * n] = outcomes[0:n] + weights[i]
-        prob_indiv[n:2 * n] = prob_indiv[0:n] / (1 - probs[i]) * probs[i]
-
-    return prob_indiv, outcomes
+    return probs, weights, sum_bias
 
 
 def bernmix_pmf_int(probs, weights, outcomes=None):
     """
-    This function reputrn the vector of probabilities for possible values
-    of the weighted sum of Bernoulli random variables
+    This function returns the PMF of the weighted sum of BRVs
     when weights are integer
     :param probs:
     :param weights:
     :param outcomes:
     :return: The PMF across all possible values
     """
-    if len(probs) != len(weights):
-        raise ValueError('Vectors of probabilities and weights must be the same length')
-    if len(probs) == 0:
-        raise ValueError('Sum of BRVs should contain at least one RV')
-    if sum((0 > probs) | (probs > 1)):
-        raise ValueError('Probabilities should be within [0 1] segment')
 
-    # ANNA
-    # if weighas are integer
-    # if outcomes are integer
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    control.lengths(weights, probs)
+    # ----------------------------------------------
 
-    probs, weights, sum_neg_weights = normalise_params(probs, weights)
+    # remove trivial terms
+    probs, weights, sum_bias = normalise_params(probs, weights)
     pmf_bm = bmi.pmf(probs, weights)
 
     if outcomes in None:
         return pmf_bm
     else:
-        return pmf_bm[outcomes + sum_neg_weights]
+        return pmf_bm[outcomes + sum_bias]
 
 
-def bernmix_cdf_double(probs, weights, target_indiv, m=10**6, l=100):
+def bernmix_cdf_int(probs, weights, outcomes=None):
+    """
+    This function returns the CDF of the weighted sum of BRVs
+    when weights are integer
+    when weights are integer
+    :param probs:
+    :param weights:
+    :param outcomes:
+    :return: The PMF across all possible values
+    """
+
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.probs(probs)
+    control.weights_dbl(weights)
+    control.lengths(weights, probs)
+    # ----------------------------------------------
+
+    # remove trivial terms
+    probs, weights, sum_bias = normalise_params(probs, weights)
+    # compute PMF
+    pmf_bm = bmi.pmf(probs, weights)
+    cdf_bm = np.cumsum(pmf_bm)
+
+    if outcomes in None:
+        return cdf_bm
+    else:
+        return cdf_bm[outcomes + sum_bias]
+
+
+def bernmix_cdf_double(probs, weights, target_indiv,
+                       m_rounding=10**6, n_solutions=None):
     """
     This function reputrn the vector of probabilities for possible values
     of the weighted sum of Bernoulli random variables
     when weights are double
-    :param probs:
-    :param weights:
-    :param target_value:
-    :param n_points: a number of point to approximate distribution
-    :return: the CDF in target_value
+    :param probs: a List of real numbers in the range [0,1]
+                 representing probabilities of BRVs
+    :param weights: a List of numbers; weights in a weighted sum of BRVs
+    :param target_indiv: a List of binary outcomes of BRVs, 0/1 numbers
+    :param m_rounding: a number of points to approximate
+                       the weighted sum of BRVs
+    :param n_solutions: a number of runs for linear integer programming
+                        to correct the CDF value
+    :return: an approximated or corrected CDF value for the target_indiv
     """
-    pass
+
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    control.individual(target_indiv)
+    control.lengths(weights, probs, target_indiv)
+    control.m_rounding(m_rounding, weights)
+    control.n_solutions(n_solutions)
+    # ----------------------------------------------
+
+    if n_solutions is None:
+        cdf_value = bmd.cdf_rounded(probs, weights, target_indiv, m_rounding)
+    else:
+        cdf_value = bmc.cdf_corrected(probs, weights, target_indiv,
+                                      m_rounding, n_solutions)
+
+    return cdf_value
 
 
-def poibinmix_pmf_int(probs, wights):
+def permut_cdf(probs, weights, target_indivs, n_permut=10 ** 6):
     """
-    Linear combination of Poisson Binomial distributions
-    :param probs:
-    :param wights:
-    :return:
+    Get CDF py permutations/simulations
+    :param probs: A list of real numbers in the range [0,1]
+                 representing probabilities of BRVs
+    :param weights: A list of numbers; weights in a weighted sum of BRVs
+    :param target_indivs: A list of individual, where each is a list of binary
+                          outcomes of BRVs, list with 0 or 1 numbers
+    :param n_permut: Number of permutations
+    :return: CDF value
     """
-    pass
+
+    # ----------------------------------------------------
+    # Control Input values
+    # ----------------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    [control.individual(indiv) for indiv in target_indivs]
+    control.lengths(weights, probs, *target_indivs)
+    control.n_solutions(n_permut)
+    # ----------------------------------------------------
+
+    # generate a population of Multivariate BRV with target probabilities
+    pop = np.zeros((len(probs), n_permut), int)  # Pre-allocate matrix
+    for i, p in enumerate(probs):
+        pop[i] = np.random.binomial(1, p, n_permut)
+    pop = np.transpose(pop)
+
+    # Compute outcomes of the weighted sum of BRVs for the population
+    pop_values = list(map(lambda indiv: np.dot(weights, indiv), pop))
+
+    # Compute outcomes of the weighted sum of BRVs for the target individuals
+    target_values = map(lambda indiv: np.dot(weights, indiv), target_indivs)
+
+    # Compute the approximation of CDF for target_values
+    cdfs = [map(lambda value: sum(pop_values <= value) / n_permut,
+                target_values)]
+
+    return cdfs
 
 
-def poibinmix_cdf_double(probs, wights, target_value, n_points = None):
+def conv_pmf_int(probs, weights):
     """
-    Linear combination of Poisson Binomial distributions
-    :param probs:
-    :param wights:
-    :param target_value:
-    :param n_points:
-    :return:
+    This function caclulates pmf fpr each individual by convolution
+    :param probs: A list of real numbers in the range [0,1]
+                 representing probabilities of BRVs
+    :param weights: A list of numbers; weights in a weighted sum of BRVs
+    :return: pmf_bm - a vector with discrete values of PMF: P(x) = pmf_bm[x]
     """
-    pass
+
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    control.lengths(weights, probs)
+    # ----------------------------------------------
+
+    prob_indiv, outcomes = conv_all_outcomes(probs, weights)
+    pmf_bm = [sum(prob_indiv[outcomes == i])
+              for i in range(0, sum(weights) + 1)]
+    return pmf_bm
 
 
-def binmix_pmf_int(probs, num_of_trails, wights):
+def conv_all_outcomes(probs, weights):
     """
-    Linear combination of Poisson Binomial distributions
-    :param probs:
-    :param num_of_trails:
-    :param wights:
-    :return:
+    This function calculates probabilities for each outcome
+    :param probs: vector of probabilities
+    :param weights: vector of weights
+    :return: probabilities and outcomes
     """
-    pass
 
-def binmix_cdf_double(probs, num_of_trails, wights, target_value, n_points = None):
-    """
-    Linear combination of Poisson Binomial distributions
-    :param probs:
-    :param num_of_trails:
-    :param wights:
-    :param target_value:
-    :param n_points:
-    :return:
-    """
-    pass
+    def comp_indiv_prob(indiv, probs):
+        """ Compute probability for an individual """
+        prob_multiply = list(map(lambda ind, p: p if ind == 1 else (1 - p),
+                                 indiv, probs))
+        return np.prod(prob_multiply)
 
-def radmix_pmf_int(probs, wights):
-    """
-    Rademacher distribution
-    :return:
-    """
-    pass
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    control.lengths(weights, probs)
+    # ----------------------------------------------
 
+    n_terms = len(probs)
+    # initialise size of outcomes
+    outcomes = np.zeros(2 ** n_terms)
+    prob_indiv = np.zeros(2 ** n_terms)
 
+    # initialise two first of outcomes (0,0,...0) and (1,0,...0)
+    outcomes[0:2] = [0, weights[0]]
+    prob_indiv[0] = comp_indiv_prob(np.zeros(n_terms), probs)
+    prob_indiv[1] = comp_indiv_prob(np.append([1], np.zeros(n_terms-1)), probs)
+
+    for i in range(1, n_terms):
+        n = 2 ** i
+        outcomes[n:2 * n] = outcomes[0:n] + weights[i]
+        prob_indiv[n:2 * n] = prob_indiv[0:n] / (1 - probs[i]) * probs[i]
+
+    return prob_indiv, outcomes
+
+#
+# def poibinmix_pmf_int(probs, wights):
+#     pass
+#
+#
+# def poibinmix_cdf_double(probs, wights, target_value, n_points = None):
+#     pass
+#
+#
+# def binmix_pmf_int(probs, num_of_trails, wights):
+#     pass
+#
+#
+# def binmix_cdf_double(probs, num_of_trails, wights, target_value,
+#                       n_points = None):
+#     pass
+#
+#
+# def radmix_pmf_int(probs, wights):
+#     pass
+#
 
 
 if __name__ == "__main__":
--- bernmix_double/bernmix_double.py
+++ bernmix_double/bernmix_double.py
@@ -1,183 +1,74 @@
+"""
+This module contain tools to compute CDF for a weighted sum of BRVs
+with non-integer weights using rounding
+"""
+
+__author__ = "Anna Igolkina"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Anna Igolkina"
+__email__ = "igolkinaanna11@gmail.com"
+
+__all__ = ["weight_rounding", "cdf_rounded"]
+
 import numpy as np
-import bernmix_int.bernmix_int as bm_int
+
+import bernmix_int.bernmix_int as bmi
+import bernmix_control as control
 
 
-
-def cdf_fixed_scaling(probs, weights, target_bern_vector, n_points):
+def weight_rounded(weights, m_points):
     """
-    This function calculates the CDF value for fixed number of points to approximate
-    :param probs:
-    :param weights:
-    :param target_value:
-    :param n_points:
+    This function return the scaled and rounded  approximation of weights
+    :param weights: a List of numbers; weights in a weighted sum of BRVs
+    :param m_points: a number of points to approximate
+                       the weighted sum of BRVs
     :return:
     """
+
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.m_rounding(m_points, weights)
+    # ----------------------------------------------
+
     n = len(weights)
-    w = sum(weights)
-    c = (n_points + n) / w
-
-    weights_new = np.around(weights * c, decimals=1).astype(int)
-    new_target_value = np.dot(weights_new, target_bern_vector).astype(int)
+    weight_sum = sum(weights)
+    c = (m_points + n) / weight_sum
+    return np.around(weights * c).astype(int)
 
 
-    pmf = bm_int.pmf(probs, weights_new)
-
-
-    return sum(pmf[:new_target_value + 1:])
-
-
-def cdfs_fixed_M(probs, weights, target_vectors, n_points):
+def cdf_rounded(probs, weights, target_indivs, m_rounding=10**6):
     """
-    This function calculates the CDF value for fixed number of points to approximate
-    :param probs:
-    :param weights:
-    :param target_value:
-    :param n_points:
-    :return:
-    """
-    n = len(weights)
-    w = sum(weights)
-    c = (n_points + n) / w
-
-    weights_new = np.around(weights * c, decimals=1).astype(int)
-    target_values = map(lambda x: np.dot(weights_new, x).astype(int), target_vectors)
-
-
-    pmf = bm_int.pmf(probs, weights_new)
-
-    pmfs = map(lambda x: sum(pmf[:x + 1:]), target_values)
-
-
-    return pmfs
-
-
-
-def cdf_range(probs, weights, target_bern_vector, n_points, k):
-    """
-    This function calculates the CDF value for fixed number of points to approximate
-    :param probs:
-    :param weights:
-    :param target_value:
-    :param n_points:
-    :return:
-    """
-    n = len(weights)
-    w = sum(weights)
-    c = (n_points + n) / w
-
-    weights_new = np.around(weights * c, decimals=1).astype(int)
-    new_target_value = np.dot(weights_new, target_bern_vector)
-
-
-    pmf = bm_int.pmf(probs, weights_new)
-
-
-    return sum(pmf[:new_target_value + 1:]), \
-           pmf[new_target_value - k: new_target_value + k + 1:]
-
-
-
-def cdf_range_scaling(probs, weights, target_indiv, n_range,
-                      n_epoch = 1000, pop_size = 1000, mut_prob = None):
-    """
-    This function calculates the CDF value for fixed number of points to approximate
-    :param probs:
-    :param weights:
-    :param target_value:
-    :param n_points:
+    This function calculates the CDF value for several individuals
+    using fixed number of points to approximate
+    :param probs: a List of real numbers in the range [0,1]
+                  representing probabilities of BRVs
+    :param weights: a List of numbers; weights in a weighted sum of BRVs
+    :param target_indivs: a list of individuals where each is a list of binary
+                          outcomes of BRVs, 0/1 numbers
+    :param m_rounding: a number of points to approximate
+                       the weighted sum of BRVs
     :return:
     """
 
+    # ----------------------------------------------
+    # Control Input values
+    # ----------------------------------------------
+    control.weights_dbl(weights)
+    control.probs(probs)
+    [control.individual(indiv) for indiv in target_indivs]
+    control.lengths(weights, probs, *target_indivs)
+    control.m_rounding(m_rounding, weights)
+    # ----------------------------------------------
 
-    # initialisation of genetic algorithm
-    mut_prob = 2/len(weights)
-    pop = ga.EvolvingPop(weights, pop_size, mut_prob)
+    # Compute approximated PMF
+    weights_new = weight_rounded(weights, m_rounding)
+    pmf = bmi.pmf(probs, weights_new)
 
-    target_value = pop.weighted_sum(target_indiv)
+    target_values = map(lambda indiv: np.dot(weights_new, indiv).astype(int),
+                        target_indivs)
+    cdfs = [map(lambda value: sum(pmf[:(value+1)]), target_values)]
 
-    pop.init_by_target(target_indiv)
-
-    pop.evolve(n_epoch, target_value)
-
-    # parameters for approximation
-
-    m1 = n_range[0]
-    m2 = n_range[1]
-    n_points, indexes_down, indexes_up = pop.approximate(m1, m2, target_value)
-
-
-    prob_discr = 0
-    prob_discr = pop.prob_discrepancy(
-                     n_points,
-                     indexes_down,
-                     indexes_up,
-                     target_indiv,
-                     probs)
-    print(prob_discr)
-
-    cdf = cdf_fixed_scaling(probs, weights, target_indiv, n_points)
-
-    return  cdf, cdf + prob_discr
-
-def cdf_range_scaling_not_corrected(probs, weights, target_indiv, n_range,
-                      n_epoch = 1000, pop_size = 1000, mut_prob = None):
-    """
-    This function calculates the CDF value for fixed number of points to approximate
-    :param probs:
-    :param weights:
-    :param target_value:
-    :param n_points:
-    :return:
-    """
-
-
-    # initialisation of genetic algorithm
-    mut_prob = 2/len(weights)
-    pop = ga.EvolvingPop(weights, pop_size, mut_prob)
-
-    target_value = pop.weighted_sum(target_indiv)
-
-    pop.evolve(n_epoch, target_value)
-
-    # parameters for approximation
-
-    m1 = n_range[0]
-    m2 = n_range[1]
-    n_points, indexes_down, indexes_up = pop.approximate(m1, m2, target_value)
-
-
-
-    return cdf_fixed_scaling(probs, weights, target_indiv, n_points)
-
-
-
-def cdf_permutations(probs, weights, target_indiv, n_permut):
-    """
-    Get CDF py permutations/simulations
-    :param probs: 
-    :param weights: 
-    :param target_indiv: 
-    :param n_permut: 
-    :return: 
-    """
-
-    pop = np.empty(shape=[0, n_permut], dtype=int)
-    for i in range(len(weights)):
-        pop = np.append(pop, np.random.binomial(1, probs[i], (1, n_permut)), axis=0)
-
-    pop = np.transpose(pop)
-
-    target_value = np.dot(weights, target_indiv)
-
-    pop_values = list(map(lambda x: np.dot(x, weights), pop))
-
-    cdf_target_value = sum(value <= target_value for value in pop_values)/n_permut
-
-    return cdf_target_value
-
-
-
-
-
-
-
+    return cdfs
